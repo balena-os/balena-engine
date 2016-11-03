52bba34ca92659709a29e589d566b4ab4984aa13
Petros Angelatos <petrosagg@gmail.com>
2016-11-03 00:38:14 +0000

    aufs,overlay: durably write layer on disk before returning
    This patch makes sure the layer contents are synced to disk before
    reporting the ApplyDiff operation as successful. This prevents
    /var/lib/docker corruption but the method used here is not the most
    efficient since it will sync all the currently mounted filesystems.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- daemon/graphdriver/aufs/aufs.go
+++ daemon/graphdriver/aufs/aufs.go
@@ -35,6 +35,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 
 	"github.com/containerd/containerd/pkg/userns"
 	"github.com/docker/docker/daemon/graphdriver"
@@ -477,6 +478,10 @@ func (a *Driver) ApplyDiff(id, parent string, diff io.Reader) (size int64, err e
 	if err = a.applyDiff(id, diff); err != nil {
 		return
 	}
+
+	// FIXME: Instead of syncing all the filesystems we should be fsyncing each
+	// file as the tar archive gets unpacked
+	syscall.Sync()
 
 	return a.DiffSize(id, parent)
 }
