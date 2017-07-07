7d3df0ceaf08b6b4b5eae1db90290049ee95eb11
Petros Angelatos <petrosagg@gmail.com>
2017-07-06 20:21:37 -0700

    make GetTarSeekStream part of the ImageConfigStore interface
    This will allow distribution code to request a seekable tar stream in
    order to compute a binary delta on top of it.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- distribution/config.go
+++ distribution/config.go
@@ -13,6 +13,7 @@ import (
 	"github.com/docker/docker/distribution/xfer"
 	"github.com/docker/docker/image"
 	"github.com/docker/docker/layer"
+	"github.com/docker/docker/pkg/ioutils"
 	"github.com/docker/docker/pkg/progress"
 	"github.com/docker/docker/pkg/system"
 	refstore "github.com/docker/docker/reference"
@@ -81,6 +82,7 @@ type ImagePushConfig struct {
 type ImageConfigStore interface {
 	Put(context.Context, []byte) (digest.Digest, error)
 	Get(context.Context, digest.Digest) ([]byte, error)
+	GetTarSeekStream(digest.Digest) (ioutils.ReadSeekCloser, error)
 }
 
 // PushLayerProvider provides layers to be pushed by ChainID.
@@ -123,6 +125,10 @@ func (s *imageConfigStore) Get(_ context.Context, d digest.Digest) ([]byte, erro
 		return nil, err
 	}
 	return img.RawJSON(), nil
+}
+
+func (s *imageConfigStore) GetTarSeekStream(d digest.Digest) (ioutils.ReadSeekCloser, error) {
+	return s.Store.GetTarSeekStream(image.IDFromDigest(d))
 }
 
 func rootFSFromConfig(c []byte) (*image.RootFS, error) {
