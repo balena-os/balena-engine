7f6595cdbb59e34b97f5e73ff9b36b4618a5f926
Petros Angelatos <petrosagg@gmail.com>
2017-07-12 22:55:20 -0700

    distribution: implement delta pull
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>
    
    This adds some context to the error message when there's some error
    loading the delta base image (for example, when the delta base image is
    not found locally).
    
    The error message we had before was generic and low-level, which didn't
    give any clues about what lead to the error.
    
    Signed-off-by: Leandro Motta Barros <leandro@balena.io>
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- distribution/pull_v2.go
+++ distribution/pull_v2.go
@@ -166,6 +166,7 @@ type layerDescriptor struct {
 	layerDownload    io.ReadCloser
 	downloadAttempts uint8
 	downloadOffset   int64
+	deltaBase        io.ReadSeeker
 }
 
 func (ld *layerDescriptor) Key() string {
@@ -229,6 +230,10 @@ func (ld *layerDescriptor) Read(p []byte) (int, error) {
 	}
 
 	return n, err
+}
+
+func (ld *layerDescriptor) DeltaBase() io.ReadSeeker {
+	return ld.deltaBase
 }
 
 func (ld *layerDescriptor) Close() {
@@ -536,6 +541,43 @@ func (p *puller) pullSchema2Layers(ctx context.Context, target distribution.Desc
 		return "", imageConfigPullError{Err: err}
 	}
 
+	var deltaBase io.ReadSeeker
+
+	// check for delta config
+	img, err := image.NewFromJSON(configJSON)
+	if err != nil {
+		return "", err
+	}
+
+	if img.Config != nil {
+		if base, ok := img.Config.Labels["io.resin.delta.base"]; ok {
+			baseDigest, err := digest.Parse(base)
+			if err != nil {
+				return "", err
+			}
+
+			stream, err := p.config.ImageStore.GetTarSeekStream(baseDigest)
+			if err != nil {
+				return "", fmt.Errorf("loading delta base image %v: %w", digest, err)
+			}
+			defer stream.Close()
+
+			deltaBase = stream
+		}
+
+		if config, ok := img.Config.Labels["io.resin.delta.config"]; ok {
+			configDigest := digest.FromString(config)
+
+			if _, err := p.config.ImageStore.Get(ctx, configDigest); err == nil {
+				// If the image already exists locally, no need to pull
+				// anything.
+				return configDigest, nil
+			}
+
+			configJSON = []byte(config)
+		}
+	}
+
 	configRootFS, err := rootFSFromConfig(configJSON)
 	if err == nil && configRootFS == nil {
 		return "", errRootFSInvalid
@@ -561,6 +603,7 @@ func (p *puller) pullSchema2Layers(ctx context.Context, target distribution.Desc
 			repoInfo:        p.repoInfo,
 			metadataService: p.metadataService,
 			src:             d,
+			deltaBase:       deltaBase,
 		}
 
 		descriptors = append(descriptors, layerDescriptor)
