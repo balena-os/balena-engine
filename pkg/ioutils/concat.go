f8d2297b28b1af73970d21eb65f9c0c9fae2297c
Petros Angelatos <petrosagg@gmail.com>
2017-07-13 16:30:08 -0700

    ioutils: implement ReadSeekCloser concat stream
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>
    
    The root cause of this issue was an integer overflow in 32-bit platforms
    -- specifically, when explicitly converting `io.Reader` offsets
    (`int64`) to slice indices (`int`, which is only 32-bit long on 32-bit
    platforms).
    
    We already had in place the checks supposed to ensure slice bounds where
    always within the expected bounds. So, in a way, this commit just
    re-organizes the code so that we perform safely this required `int64` to
    `int` type conversion.
    
    Signed-off-by: Leandro Motta Barros <leandro@balena.io>
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ pkg/ioutils/concat.go
@@ -0,0 +1,134 @@
+package ioutils
+
+import (
+	"errors"
+	"io"
+)
+
+// clampSliceIndex clamps index to be between min and max (inclusive). As the
+// name suggests, the function is designed to be especially useful for slice
+// indices -- and in particular, to do so safely in both 32- and 64-bit
+// platforms.
+func clampSliceIndex(index int64, min, max int) int {
+	if index < int64(min) {
+		return min
+	}
+	if index > int64(max) {
+		return max
+	}
+	return int(index)
+}
+
+// max64 returns the larger of two int64 values.
+func max64(x, y int64) int64 {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func SeekerSize(s io.Seeker) (int64, error) {
+	cur, err := s.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, err
+	}
+
+	size, err := s.Seek(0, io.SeekEnd)
+	if err != nil {
+		return 0, err
+	}
+
+	if _, err := s.Seek(cur, io.SeekStart); err != nil {
+		return 0, err
+	}
+
+	return size, nil
+}
+
+type concatReadSeekCloser struct {
+	a     ReadSeekCloser
+	aSize int64
+	b     ReadSeekCloser
+	bSize int64
+	off   int64
+}
+
+func (self *concatReadSeekCloser) Read(p []byte) (n int, err error) {
+	// if the read starts within a
+	if self.off < self.aSize {
+		if _, err := self.a.Seek(self.off, io.SeekStart); err != nil {
+			return 0, err
+		}
+
+		i := clampSliceIndex(self.aSize-self.off, 0, len(p))
+		nA, err := io.ReadFull(self.a, p[:i])
+
+		if err != nil {
+			return 0, err
+		}
+		n += nA
+	}
+
+	// if the read ends within b
+	if self.off+int64(len(p)) >= self.aSize {
+		if _, err := self.b.Seek(max64(0, self.off-self.aSize), io.SeekStart); err != nil {
+			return 0, err
+		}
+
+		i := clampSliceIndex(self.aSize-self.off, 0, len(p))
+		nB, err := io.ReadFull(self.b, p[i:])
+		if err != nil {
+			return 0, err
+		}
+		n += nB
+	}
+
+	self.off += int64(n)
+
+	return
+}
+
+func (self *concatReadSeekCloser) Seek(offset int64, whence int) (int64, error) {
+	switch whence {
+	default:
+		return 0, errors.New("Seek: invalid whence")
+	case io.SeekStart:
+		break
+	case io.SeekCurrent:
+		offset += self.off
+	case io.SeekEnd:
+		offset += self.aSize + self.bSize
+	}
+	if offset < 0 {
+		return 0, errors.New("Seek: invalid offset")
+	}
+	self.off = offset
+	return offset, nil
+}
+
+func (self *concatReadSeekCloser) Close() error {
+	err1 := self.a.Close()
+	err2 := self.b.Close()
+
+	if err1 != nil {
+		return err1
+	}
+	if err2 != nil {
+		return err2
+	}
+	return nil
+}
+
+func ConcatReadSeekClosers(a, b ReadSeekCloser) (ReadSeekCloser, error) {
+	aSize, err := SeekerSize(a)
+	if err != nil {
+		return nil, err
+	}
+
+	bSize, err := SeekerSize(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return &concatReadSeekCloser{a, aSize, b, bSize, 0}, nil
+}
