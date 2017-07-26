6e257f07dfd7f928251250f99adca929e0c6197b
Petros Angelatos <petrosagg@gmail.com>
2017-07-25 17:09:09 -0700

    cmd/rce: adapt imports to new packages
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- cmd/rce/rce.go
+++ cmd/rce/rce.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/docker/containerd/containerd"
-	containerdShim "github.com/docker/containerd/containerd-shim"
-	"github.com/docker/containerd/ctr"
-	"github.com/docker/docker/cmd/docker"
+	"github.com/containerd/containerd/cmd/containerd"
+	containerdShim "github.com/containerd/containerd/cmd/containerd-shim"
+	"github.com/containerd/containerd/cmd/ctr"
+	"github.com/docker/cli/cmd/docker"
 	"github.com/docker/docker/cmd/dockerd"
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/docker/libnetwork/cmd/proxy"
