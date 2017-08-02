ae389d98303b0d4be3086610d4e0dd0130233aca
Petros Angelatos <petrosagg@gmail.com>
2017-08-01 17:15:01 -0700

    cmd/mobynit: accept a flag for the graph driver
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>
    
    Since this is PID 1 it's easier to parameterise it through a file on the
    disk rather than having to rely on the bootloader to pass the right
    arguments
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- cmd/mobynit/main.go
+++ cmd/mobynit/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	_ "github.com/docker/docker/daemon/graphdriver/aufs"
@@ -19,7 +20,7 @@ const (
 	PIVOT_PATH = "/mnt/sysroot/active"
 )
 
-func mountContainer(containerID string) string {
+func mountContainer(containerID, graphDriver string) string {
 	if err := os.MkdirAll("/dev/shm", os.ModePerm); err != nil {
 		log.Fatal("creating /dev/shm failed:", err)
 	}
@@ -64,7 +65,14 @@ func mountContainer(containerID string) string {
 }
 
 func main() {
-	flag.Parse()
+	// Any mounts done by initrd will be transfered in the new root
+	mounts, err := mount.GetMounts(nil)
+
+	rawGraphDriver, err := ioutil.ReadFile("/current/boot/storage-driver")
+	if err != nil {
+		log.Fatal("could not get storage driver:", err)
+	}
+	graphDriver := strings.TrimSpace(string(rawGraphDriver))
 
 	current, err := os.Readlink("/current")
 	if err != nil {
@@ -76,7 +84,7 @@ func main() {
 		log.Fatal("error remounting root as read/write:", err)
 	}
 
-	newRoot := mountContainer(containerID)
+	newRoot := mountContainer(containerID, graphDriver)
 
 	if err := syscall.PivotRoot(newRoot, filepath.Join(newRoot, PIVOT_PATH)); err != nil {
 		log.Fatal("error while pivoting root:", err)
