051e85c23ad55d68261270e4f1d4268dacbc6001
Petros Angelatos <petrosagg@gmail.com>
2017-08-03 19:55:56 -0700

    pkg/archive: use fadvise to prevent pagecache thrashing
    During a docker pull a very large amount of files are touched during
    unpacking. This causes linux to fill up the page cache with entries that
    most probably won't be used.
    
    There are two issues with this. The first one is that by putting a lot
    of pressure on the page cache memory fragmentation occurs. This can
    cause filesystem corruption on some platforms that can't handle memory
    allocation failures correctly.
    
    The second issue is that by not hinting our intentions to the kernel, we
    might evict useful pages from the cache that could be in use by some
    running container, and therefore affecting the performance of the
    application.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>
    
    Linux ignores fadvise for dirty pages so we need to make sure we sync to
    have them marked as clean before discarding
    
    Ref: https://github.com/torvalds/linux/blob/v4.13/mm/truncate.c#L489-L493
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>
    
    This improves the previous attempt at reducing page cache thrashing
    introduced in 8c0ceab ("pkg/archive: use fadvise to prevent pagecache
    thrashing"). While the approached worked, it introduced severe delays
    during pull especially on high latency hard drives since it had to wait
    for every file.
    
    A much better approach implemented in this commit is to run the
    unpacking process in a memory constrained cgroup to limit the maximum
    page cache usage while at the same time hinting the kernel to start the
    writeback as soon as possible to reduce the amount of dirty pages as
    much as possible.
    
    This makes `balena pull` as fast as `docker pull && sync` while having
    minimal impact on page cache usage.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -710,11 +710,15 @@ func createTarFile(path, extractDir string, hdr *tar.Header, reader io.Reader, o
 		if err != nil {
 			return err
 		}
-		if _, err := io.Copy(file, reader); err != nil {
+
+		efw := ioutils.NewEagerFileWriter(file)
+
+		if _, err := io.Copy(efw, reader); err != nil {
 			file.Close()
 			return err
 		}
-		file.Close()
+
+		efw.Close()
 
 	case tar.TypeBlock, tar.TypeChar:
 		if inUserns { // cannot create devices in a userns
@@ -830,6 +834,7 @@ func createTarFile(path, extractDir string, hdr *tar.Header, reader io.Reader, o
 			return err
 		}
 	}
+
 	return nil
 }
 
