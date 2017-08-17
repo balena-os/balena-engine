7798ec3f485f427b6dd18e0febdbf1689d97f158
Petros Angelatos <petrosagg@gmail.com>
2017-08-17 07:25:00 -0700

    distribution: check for nil before closing the download

--- distribution/pull_v2.go
+++ distribution/pull_v2.go
@@ -220,7 +220,9 @@ func (ld *v2LayerDescriptor) Read(p []byte) (int, error) {
 }
 
 func (ld *v2LayerDescriptor) Close() {
-	ld.layerDownload.Close()
+	if ld.layerDownload != nil {
+		ld.layerDownload.Close()
+	}
 }
 
 func (ld *v2LayerDescriptor) Download(ctx context.Context, progressOutput progress.Output) (io.ReadCloser, int64, error) {
