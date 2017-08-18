695cc8123bbed184e66f7e892e13bf654d1ea4b1
Petros Angelatos <petrosagg@gmail.com>
2017-08-17 20:09:44 -0700

    vendor: update vendored dependencies for removed swarm
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- vendor/github.com/docker/cli/cli/command/commands/commands.go
+++ vendor/github.com/docker/cli/cli/command/commands/commands.go
@@ -4,18 +4,10 @@ import (
 	"os"
 
 	"github.com/docker/cli/cli/command"
-	"github.com/docker/cli/cli/command/checkpoint"
-	"github.com/docker/cli/cli/command/config"
 	"github.com/docker/cli/cli/command/container"
 	"github.com/docker/cli/cli/command/image"
 	"github.com/docker/cli/cli/command/network"
-	"github.com/docker/cli/cli/command/node"
-	"github.com/docker/cli/cli/command/plugin"
 	"github.com/docker/cli/cli/command/registry"
-	"github.com/docker/cli/cli/command/secret"
-	"github.com/docker/cli/cli/command/service"
-	"github.com/docker/cli/cli/command/stack"
-	"github.com/docker/cli/cli/command/swarm"
 	"github.com/docker/cli/cli/command/system"
 	"github.com/docker/cli/cli/command/volume"
 	"github.com/spf13/cobra"
@@ -24,12 +16,6 @@ import (
 // AddCommands adds all the commands from cli/command to the root command
 func AddCommands(cmd *cobra.Command, dockerCli *command.DockerCli) {
 	cmd.AddCommand(
-		// checkpoint
-		checkpoint.NewCheckpointCommand(dockerCli),
-
-		// config
-		config.NewConfigCommand(dockerCli),
-
 		// container
 		container.NewContainerCommand(dockerCli),
 		container.NewRunCommand(dockerCli),
@@ -38,36 +24,17 @@ func AddCommands(cmd *cobra.Command, dockerCli *command.DockerCli) {
 		image.NewImageCommand(dockerCli),
 		image.NewBuildCommand(dockerCli),
 
-		// node
-		node.NewNodeCommand(dockerCli),
-
 		// network
 		network.NewNetworkCommand(dockerCli),
-
-		// plugin
-		plugin.NewPluginCommand(dockerCli),
 
 		// registry
 		registry.NewLoginCommand(dockerCli),
 		registry.NewLogoutCommand(dockerCli),
 		registry.NewSearchCommand(dockerCli),
 
-		// secret
-		secret.NewSecretCommand(dockerCli),
-
-		// service
-		service.NewServiceCommand(dockerCli),
-
 		// system
 		system.NewSystemCommand(dockerCli),
 		system.NewVersionCommand(dockerCli),
-
-		// stack
-		stack.NewStackCommand(dockerCli),
-		stack.NewTopLevelDeployCommand(dockerCli),
-
-		// swarm
-		swarm.NewSwarmCommand(dockerCli),
 
 		// volume
 		volume.NewVolumeCommand(dockerCli),
