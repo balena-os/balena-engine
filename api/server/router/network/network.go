594d5ef05ae7feafd73c130320c6b9b658fc2555
Petros Angelatos <petrosagg@gmail.com>
2017-08-17 19:34:34 -0700

    router/network: remove swarm dependency
    Network commands are built to query both the active cluster (if any) and
    the daemon. This commits makes them only deal with local networks.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- api/server/router/network/network.go
+++ api/server/router/network/network.go
@@ -2,21 +2,18 @@ package network
 
 import (
 	"github.com/docker/docker/api/server/router"
-	"github.com/docker/docker/daemon/cluster"
 )
 
 // networkRouter is a router to talk with the network controller
 type networkRouter struct {
 	backend Backend
-	cluster *cluster.Cluster
 	routes  []router.Route
 }
 
 // NewRouter initializes a new network router
-func NewRouter(b Backend, c *cluster.Cluster) router.Router {
+func NewRouter(b Backend) router.Router {
 	r := &networkRouter{
 		backend: b,
-		cluster: c,
 	}
 	r.initRoutes()
 	return r
