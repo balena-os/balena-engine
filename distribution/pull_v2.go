cd6f97d10a7a825931b5a385bc94ffb0ebbc6838
Petros Angelatos <petrosagg@gmail.com>
2017-10-03 16:13:43 -0700

    distribution: calculate combined progress when pulling
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- distribution/pull_v2.go
+++ distribution/pull_v2.go
@@ -249,6 +249,10 @@ func (ld *v2LayerDescriptor) Registered(diffID layer.DiffID) {
 	ld.V2MetadataService.Add(diffID, metadata.V2Metadata{Digest: ld.digest, SourceRepository: ld.repoInfo.Name.Name()})
 }
 
+func (ld *v2LayerDescriptor) Size() int64 {
+	return ld.src.Size
+}
+
 func (p *v2Puller) pullV2Tag(ctx context.Context, ref reference.Named, platform *specs.Platform) (tagUpdated bool, err error) {
 
 	var (
