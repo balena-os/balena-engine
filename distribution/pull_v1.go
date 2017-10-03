5da9704f836d14441b3f5f84a48ec1ec9ab7aaa9
Petros Angelatos <petrosagg@gmail.com>
2017-10-03 16:13:43 -0700

    distribution: calculate combined progress when pulling
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Yossi Eliaz <yossi@resin.io>

--- distribution/pull_v1.go
+++ distribution/pull_v1.go
@@ -369,3 +369,7 @@ func (ld *v1LayerDescriptor) Registered(diffID layer.DiffID) {
 	// Cache mapping from this layer's DiffID to the blobsum
 	ld.v1IDService.Set(ld.v1LayerID, ld.indexName, diffID)
 }
+
+func (ld *v1LayerDescriptor) Size() int64 {
+	return ld.layerSize
+}
