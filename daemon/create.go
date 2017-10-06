b9335139d1dda874dd56faa7c7260f26b4d722df
Petros Angelatos <petrosagg@gmail.com>
2017-10-06 16:38:05 -0700

    delta: revert automatic tag generation for delta image
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- daemon/create.go
+++ daemon/create.go
@@ -26,7 +26,6 @@ import (
 	"github.com/docker/docker/pkg/streamformatter"
 	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/runconfig"
-	"github.com/opencontainers/go-digest"
 	"github.com/opencontainers/selinux/go-selinux/label"
 	"github.com/pkg/errors"
 	"github.com/balena-os/librsync-go"
@@ -537,16 +536,6 @@ func (daemon *Daemon) DeltaCreate(deltaSrc, deltaDest string, outStream io.Write
 
 	id, err := is.Create(rawConfig)
 	if err != nil {
-		return err
-	}
-
-	ref, _ := reference.WithName("delta")
-
-	deltaTag := "delta-" + digest.FromString(srcImg.ID().String() + "-" + dstImg.ImageID()).Hex()[:8]
-
-	ref2, _ := reference.WithTag(ref, deltaTag)
-
-	if err := daemon.TagImageWithReference(id, "linux", ref2); err != nil {
 		return err
 	}
 
