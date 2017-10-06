385bc86e9096a6aa62575e1b6f612ee842a7492b
Petros Angelatos <petrosagg@gmail.com>
2017-10-05 22:34:13 -0700

    rename container engine to balena
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- cli/config/configdir.go
+++ cli/config/configdir.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	configDir     = os.Getenv("DOCKER_CONFIG")
-	configFileDir = ".docker"
+	configFileDir = ".balena"
 )
 
 // Dir returns the path to the configuration directory as specified by the DOCKER_CONFIG environment variable.
