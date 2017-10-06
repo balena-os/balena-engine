6ed6f42d01a997ea0f5a3a891399ae010b365a80
Petros Angelatos <petrosagg@gmail.com>
2017-10-06 14:10:06 -0700

    api: move delta creation under POST images/delta
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- api/server/router/image/backend.go
+++ api/server/router/image/backend.go
@@ -21,6 +21,7 @@ type Backend interface {
 }
 
 type imageBackend interface {
+	DeltaCreate(deltaSrc, deltaDest string, outStream io.Writer) error
 	ImageDelete(imageRef string, force, prune bool) ([]types.ImageDeleteResponseItem, error)
 	ImageHistory(imageName string) ([]*image.HistoryResponseItem, error)
 	Images(ctx context.Context, opts types.ImageListOptions) ([]*types.ImageSummary, error)
