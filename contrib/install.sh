9d913a1b788af4cc97f4d12848b545636062905b
Petros Angelatos <petrosagg@gmail.com>
2017-10-12 16:44:09 -0700

    balena build scripts
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

---
+++ contrib/install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+tag="17.06-rc5"
+tag=$(echo "$tag" | sed 's|+|.|g')
+
+machine=$(uname -m)
+
+case "$machine" in
+	"armv5"*)
+		arch="armv5"
+		;;
+	"armv6"*)
+		arch="armv6"
+		;;
+	"armv7"*)
+		arch="armv7"
+		;;
+	"armv8"*)
+		arch="aarch64"
+		;;
+	"aarch64"*)
+		arch="aarch64"
+		;;
+	"i386")
+		arch="i386"
+		;;
+	"i686")
+		arch="i386"
+		;;
+	"x86_64")
+		arch="x86_64"
+		;;
+	*)
+		echo "Unknown machine type: $machine"
+		exit 1
+esac
+
+url="https://github.com/resin-os/balena/releases/download/${tag}/balena-${tag}-${arch}.tar.gz"
+
+# curl -sL "$url" | sudo tar xzv -C /usr/local/bin --strip-components=1
+
+cat <<EOF
+
+
+   Installation successful!
+ _           _
+| |__   __ _| | ___ _ __   __ _
+| '_ \\ / _\` | |/ _ \\ '_ \ / _\` |
+| |_) | (_| | |  __/ | | | (_| |
+|_.__/ \__,_|_|\___|_| |_|\__,_|
+
+the container engine for the IoT
+EOF
