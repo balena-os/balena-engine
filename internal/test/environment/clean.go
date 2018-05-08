7c2d12f9819031880293b6fd5d5003e451b9f5d7
Petros Angelatos <petrosagg@gmail.com>
2018-05-07 20:12:52 -0700

    tests: remove plugin support in tests
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- internal/test/environment/clean.go
+++ internal/test/environment/clean.go
@@ -29,9 +29,6 @@ func (e *Execution) Clean(t assert.TestingT) {
 	deleteAllImages(t, client, e.protectedElements.images)
 	deleteAllVolumes(t, client, e.protectedElements.volumes)
 	deleteAllNetworks(t, client, platform, e.protectedElements.networks)
-	if platform == "linux" {
-		deleteAllPlugins(t, client, e.protectedElements.plugins)
-	}
 }
 
 func unpauseAllContainers(t assert.TestingT, client client.ContainerAPIClient) {
--- internal/test/environment/protect.go
+++ internal/test/environment/protect.go
@@ -41,9 +41,6 @@ func ProtectAll(t assert.TestingT, testEnv *Execution) {
 	ProtectImages(t, testEnv)
 	ProtectNetworks(t, testEnv)
 	ProtectVolumes(t, testEnv)
-	if testEnv.OSType == "linux" {
-		ProtectPlugins(t, testEnv)
-	}
 }
 
 // ProtectContainer adds the specified container(s) to be protected in case of
