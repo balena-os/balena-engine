ba9b8d82bbe1b5260f94d250d684c4f8dec55868
Robert Günzler <r@gnzler.io>
2018-12-13 20:01:58 +0100

    Allow tagging of image deltas on creation
    Similar to how the build command allows tagging of images this allows
    specifying a repo:tag indentifier to tag the delta with
    
    Requires: https://github.com/balena-os/balena-engine-cli/pull/7
    Change-type: minor
    Signed-off-by: Robert Günzler <robertg@balena.io>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- daemon/create.go
+++ daemon/create.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/docker/distribution/reference"
 	"github.com/containerd/containerd/platforms"
 	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types"
@@ -351,7 +352,7 @@ func maximumSpec() v1.Platform {
 
 // DeltaCreate creates a delta of the specified src and dest images
 // This is called directly from the Engine API
-func (daemon *Daemon) DeltaCreate(deltaSrc, deltaDest string, outStream io.Writer) error {
+func (daemon *Daemon) DeltaCreate(deltaSrc, deltaDest string, options types.ImageDeltaOptions, outStream io.Writer) error {
 	progressOutput := streamformatter.NewJSONProgressOutput(outStream, false)
 
 	srcImg, err := daemon.GetImage(deltaSrc)
@@ -555,5 +556,27 @@ func (daemon *Daemon) DeltaCreate(deltaSrc, deltaDest string, outStream io.Write
 
 	outStream.Write(streamformatter.FormatStatus("", "Normal size: %s, Delta size: %s, %.2fx improvement", humanTotal, humanDelta, deltaRatio))
 	outStream.Write(streamformatter.FormatStatus("", "Created delta: %s", id.String()))
+
+	if options.Tag == "" {
+		return nil
+	}
+
+	ref, err := reference.ParseNormalizedNamed(options.Tag)
+	if err != nil {
+		return err
+	}
+
+	if _, isCanonical := ref.(reference.Canonical); isCanonical {
+		return errors.New("build tag cannot contain a digest")
+	}
+
+	ref = reference.TagNameOnly(ref)
+
+	if err := daemon.TagImageWithReference(id, runtime.GOOS, ref); err != nil {
+		return err
+	}
+	logrus.Debugf("Tagged delta %s with %s", id.String(), reference.FamiliarString(ref))
+	outStream.Write(streamformatter.FormatStatus("", "Successfully tagged %s\n", reference.FamiliarString(ref)))
+
 	return nil
 }
