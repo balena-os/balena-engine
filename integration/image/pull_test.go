f2bfaa3910b38b308446e0de47d5bc1b642c99ea
Robert Günzler <r@gnzler.io>
2019-04-19 16:37:07 +0200

    integration-tests: Add image pull tests
    Download an image on aufs/overlay2 once with syncDiffs enabled and
    disabled, comparing the speeds and checking that without syncing is
    faster.
    
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- integration/image/pull_test.go
+++ integration/image/pull_test.go
@@ -2,11 +2,14 @@ package image
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/versions"
 	"github.com/docker/docker/errdefs"
+	"github.com/docker/docker/testutil/daemon"
+
 	"gotest.tools/v3/assert"
 	"gotest.tools/v3/skip"
 )
@@ -21,4 +24,28 @@ func TestImagePullPlatformInvalid(t *testing.T) {
 	assert.Assert(t, err != nil)
 	assert.ErrorContains(t, err, "unknown operating system or architecture")
 	assert.Assert(t, errdefs.IsInvalidParameter(err))
+}
+
+func TestImagePullNoSyncDiffsOverlay2(t *testing.T) {
+	skip.If(t, testEnv.IsRemoteDaemon)
+
+	testPullWithSyncDiffs(t, false)
+}
+
+func testPullWithSyncDiffs(t *testing.T, syncDiffs bool) {
+	// should have a few layers so it actually has an impact on pull performance
+	testImage := "balenalib/amd64-debian:build"
+
+	var args = []string{
+		"--storage-driver=overlay2",
+		fmt.Sprintf("--storage-opt=overlay2.sync_diffs=false", syncDiffs),
+	}
+
+	d := daemon.New(t)
+	d.Start(t, args...)
+	defer d.Stop(t)
+
+	client := d.NewClientT(t)
+	_, err := client.ImagePull(context.Background(), testImage, types.ImagePullOptions{})
+	assert.NilError(t, err)
 }
