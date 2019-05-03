1125bf82a8d4d012263fca2c2d793462af99283f
Robert Günzler <r@gnzler.io>
2019-05-03 14:23:11 +0000

    Add aufs to overlay2 migrator
    The main logic is under pkg/storagemigration. This is able to seamlessly
    migrate images and containers from AUFS to overlay2.
    
    Change-type: patch
    Signed-off-by: Robert Günzler <robertg@balena.io>
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ pkg/storagemigration/failcleanup.go
@@ -0,0 +1,40 @@
+package storagemigration
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+)
+
+// FailCleanup should be run after a failed migration.
+// It will remove any files left over from the migration process
+// and migrate containers back to aufs.
+//
+func FailCleanup(root string) error {
+	logrus.WithField("storage_root", root).Warning("recovering from failed aufs to overlay migration")
+
+	var err error
+	err = SwitchAllContainersStorageDriver(root, "aufs")
+	if err != nil {
+		return fmt.Errorf("Error migrating containers to aufs: %v", err)
+	}
+
+	err = removeDirIfExists(tempTargetRoot(root))
+	if err != nil {
+		return err
+	}
+
+	err = removeDirIfExists(overlayRoot(root))
+	if err != nil {
+		return err
+	}
+
+	overlayImageDir := filepath.Join(root, "image", "overlay2")
+	err = removeDirIfExists(overlayImageDir)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
