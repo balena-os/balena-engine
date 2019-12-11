resin-io/docker

Renamed the target and binary name
In order to avoid conflict with previous code.
We renamed the rce into rce-docker.

Makefile
@@ -80,8 +80,8 @@ all: build ## validate all checks, build linux binaries, run all tests\ncross bu
 binary: build ## build the linux binaries
 	$(DOCKER_RUN_DOCKER) hack/make.sh binary
 
-rce: build ## build the linux consalidate binary
-	$(DOCKER_RUN_DOCKER) hack/make.sh binary-rce
+rce-docker: build ## build the linux consalidate binary
+	$(DOCKER_RUN_DOCKER) hack/make.sh binary-rce-docker
 
 build: bundles init-go-pkg-cache
 	docker build ${BUILD_APT_MIRROR} ${DOCKER_BUILD_ARGS} -t "$(DOCKER_IMAGE)" -f "$(DOCKERFILE)" .

balena-os/balena-engine

Makefile
@@ -136,8 +136,8 @@ all: build ## validate all checks, build linux binaries, run all tests\ncross bu
 binary: build ## build the linux binaries
 	$(DOCKER_RUN_DOCKER) hack/make.sh binary
 
-rce: build ## build the linux consolidate binary
-	$(DOCKER_RUN_DOCKER) hack/make.sh binary-rce
+rce-docker: build ## build the linux consolidate binary
+	$(DOCKER_RUN_DOCKER) hack/make.sh binary-rce-docker
 
 
 