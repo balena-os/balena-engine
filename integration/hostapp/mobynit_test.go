417ab07a6da45d6ddfa07b4a34f5b0abc001bbcd
Robert Günzler <r@gnzler.io>
2020-04-27 12:16:05 +0200

    Add integration tests for hostapp handling
    This refactors mobynit to allow for testing parts of it from
    the integration tests.
    Also adds some sanity checks for "bare" containers.
    
    Change-type: patch
    Signed-off-by: Robert Günzler <robertg@balena.io>

---
+++ integration/hostapp/mobynit_test.go
@@ -0,0 +1,52 @@
+package hostapp
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/cmd/mobynit/hostapp"
+	"github.com/docker/docker/internal/test/daemon"
+
+	"gotest.tools/assert"
+	"gotest.tools/skip"
+)
+
+func TestMobynitMountContainer(t *testing.T) {
+	skip.If(t, testEnv.DaemonInfo.OSType != "linux")
+	skip.If(t, testEnv.IsRemoteDaemon, "cannot start daemon on remote test run")
+	defer setupTest(t)()
+
+	d := daemon.New(t)
+	d.StartWithBusybox(t)
+	defer d.Stop(t)
+
+	client := d.NewClientT(t)
+
+	c, err := client.ContainerCreate(context.Background(),
+		&container.Config{Image: "busybox:latest"},
+		&container.HostConfig{Runtime: "bare"},
+		&network.NetworkingConfig{},
+		"",
+	)
+	assert.NilError(t, err)
+
+	var (
+		layerRoot     = d.RootDir()
+		storageDriver = d.StorageDriver()
+		containerID   = c.ID
+	)
+	newRootPath := hostapp.MountContainer(layerRoot, containerID, storageDriver)
+
+	// give the daemon's layer store cleanup goroutine some time to run
+	// TODO probably use gotest.tools/poll
+	time.Sleep(1 * time.Second)
+
+	// only checks if the target mount point exists
+	fi, err := os.Stat(newRootPath)
+	assert.NilError(t, err)
+	assert.Check(t, fi.IsDir())
+}
