9ceca558366ba5a31539719e7a825a1272561c20
Robert Günzler <r@gnzler.io>
2020-04-27 12:16:05 +0200

    Add integration tests for hostapp handling
    This refactors mobynit to allow for testing parts of it from
    the integration tests.
    Also adds some sanity checks for "bare" containers.
    
    Change-type: patch
    Signed-off-by: Robert Günzler <robertg@balena.io>

---
+++ integration/hostapp/bare_runtime_test.go
@@ -0,0 +1,42 @@
+package hostapp
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/testutil/request"
+
+	"gotest.tools/v3/assert"
+	"gotest.tools/v3/skip"
+)
+
+func TestBareRuntime(t *testing.T) {
+	skip.If(t, testEnv.DaemonInfo.OSType != "linux")
+	skip.If(t, testEnv.IsRemoteDaemon, "cannot start daemon on remote test run")
+	defer setupTest(t)()
+
+	client := request.NewAPIClient(t)
+	ctx := context.Background()
+
+	c, err := client.ContainerCreate(ctx,
+		&container.Config{Image: "busybox:latest"},
+		&container.HostConfig{Runtime: "bare"},
+		&network.NetworkingConfig{},
+		nil,
+		"",
+	)
+	assert.NilError(t, err)
+
+	j, err := client.ContainerInspect(ctx, c.ID)
+	assert.NilError(t, err)
+
+	containerDir, ok := j.GraphDriver.Data["MergedDir"]
+	assert.Check(t, ok)
+
+	_, err = os.Stat(filepath.Join(containerDir, ".dockerenv"))
+	assert.Check(t, os.IsNotExist(err))
+}
