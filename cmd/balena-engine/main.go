6867cb3af1e5463e28f3e3ef60607bd6660c6a34
Robert Günzler <r@gnzler.io>
2021-08-25 16:16:29 +0200

    Actually fully use pkg/reexec
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- cmd/balena-engine/main.go
+++ cmd/balena-engine/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/containerd/containerd/cmd/containerd"
 	containerdShim "github.com/containerd/containerd/cmd/containerd-shim"
 	"github.com/containerd/containerd/cmd/ctr"
@@ -11,34 +13,42 @@ import (
 	"github.com/docker/libnetwork/cmd/proxy"
 	"github.com/opencontainers/runc"
 
-	"os"
 	filepath "path/filepath"
 )
 
+func init() {
+	reexec.Register("balena-engine", docker.Main)
+	reexec.Register("balena", docker.Main)
+
+	reexec.Register("balena-engine-daemon", dockerd.Main)
+	reexec.Register("balenad", dockerd.Main)
+
+	reexec.Register("containerd", containerd.Main)
+	reexec.Register("balena-engine-containerd", containerd.Main)
+	reexec.Register("balena-containerd", containerd.Main)
+
+	reexec.Register("containerd-shim", containerdShim.Main)
+	reexec.Register("balena-engine-containerd-shim", containerdShim.Main)
+	reexec.Register("balena-containerd-shim", containerdShim.Main)
+
+	reexec.Register("ctr", ctr.Main)
+	reexec.Register("balena-engine-containerd-ctr", ctr.Main)
+	reexec.Register("balena-containerd-ctr", ctr.Main)
+
+	reexec.Register("balena-proxy", proxy.Main)
+	reexec.Register("balena-engine-proxy", proxy.Main)
+
+	reexec.Register("runc", runc.Main)
+	reexec.Register("balena-engine-runc", runc.Main)
+	reexec.Register("balena-runc", runc.Main)
+}
+
 func main() {
+	os.Args[0] = filepath.Base(os.Args[0])
 	if reexec.Init() {
 		return
 	}
 
-	command := filepath.Base(os.Args[0])
-
-	switch command {
-	case "balena", "balena-engine":
-		docker.Main()
-	case "balenad", "balena-engine-daemon":
-		dockerd.Main()
-	case "balena-containerd", "balena-engine-containerd":
-		containerd.Main()
-	case "balena-containerd-shim", "balena-engine-containerd-shim":
-		containerdShim.Main()
-	case "balena-containerd-ctr", "balena-engine-containerd-ctr":
-		ctr.Main()
-	case "balena-runc", "balena-engine-runc":
-		runc.Main()
-	case "balena-proxy", "balena-engine-proxy":
-		proxy.Main()
-	default:
-		fmt.Fprintf(os.Stderr, "error: unkown command: %v\n", command)
-		os.Exit(1)
-	}
+	fmt.Fprintf(os.Stderr, "reexec failed: unkown command: %v\n", os.Args[0])
+	os.Exit(1)
 }
