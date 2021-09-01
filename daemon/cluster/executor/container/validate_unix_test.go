94932b558d2ae32a9805d97dba2783b104ecbcbd
Robert Günzler <r@gnzler.io>
2021-09-01 20:39:59 +0200

    Drop daemon/cluster package
    Signed-off-by: Robert Günzler <robertg@balena.io>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- daemon/cluster/executor/container/validate_unix_test.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-// +build !windows
-
-package container // import "github.com/docker/docker/daemon/cluster/executor/container"
-
-const (
-	testAbsPath        = "/foo"
-	testAbsNonExistent = "/some-non-existing-host-path/"
-)
