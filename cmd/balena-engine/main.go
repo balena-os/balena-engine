e02cfb68aff657f97f73fc02552482ed01f5e646
Robert Günzler <r@gnzler.io>
2022-03-02 13:45:19 +0100

    Allow reexecing runtime shim v2
    Change-type: patch
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- cmd/balena-engine/main.go
+++ cmd/balena-engine/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/containerd/containerd/cmd/containerd"
 	containerdShim "github.com/containerd/containerd/cmd/containerd-shim"
 	"github.com/containerd/containerd/cmd/ctr"
+	containerdRuntimev2 "github.com/containerd/containerd/runtime/v2/runc/v2"
+	containerdRuntimeShim "github.com/containerd/containerd/runtime/v2/shim"
 	"github.com/docker/cli/cmd/docker"
 	"github.com/docker/docker/cmd/dockerd"
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/docker/libnetwork/cmd/proxy"
 	"github.com/opencontainers/runc"
-
-	"os"
-	filepath "path/filepath"
 )
 
 func main() {
@@ -27,13 +29,15 @@ func main() {
 		docker.Main()
 	case "balenad", "balena-engine-daemon":
 		dockerd.Main()
-	case "balena-containerd", "balena-engine-containerd":
+	case "balena-containerd", "balena-engine-containerd", "containerd":
 		containerd.Main()
 	case "balena-containerd-shim", "balena-engine-containerd-shim":
 		containerdShim.Main()
+	case "containerd-shim-runc-v2":
+		containerdRuntimeShim.Run("io.containerd.runc.v2", containerdRuntimev2.New)
 	case "balena-containerd-ctr", "balena-engine-containerd-ctr":
 		ctr.Main()
-	case "balena-runc", "balena-engine-runc":
+	case "balena-runc", "balena-engine-runc", "runc":
 		runc.Main()
 	case "balena-proxy", "balena-engine-proxy":
 		proxy.Main()
