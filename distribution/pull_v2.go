8779140291dd09be406f0780e5ee7ec2cb0861fa
Leandro Motta Barros <lmb@stackedboxes.org>
2022-06-03 13:34:28 -0300

    Improve error reporting for delta base image loading
    This adds some context to the error message when there's some error
    loading the delta base image (for example, when the delta base image is
    not found locally).
    
    The error message we had before was generic and low-level, which didn't
    give any clues about what lead to the error.
    
    Signed-off-by: Leandro Motta Barros <leandro@balena.io>
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- distribution/pull_v2.go
+++ distribution/pull_v2.go
@@ -581,7 +581,7 @@ func (p *puller) pullSchema2Layers(ctx context.Context, target distribution.Desc
 
 			stream, err := p.config.ImageStore.GetTarSeekStream(digest)
 			if err != nil {
-				return "", err
+				return "", fmt.Errorf("loading delta base image %v: %w", digest, err)
 			}
 			defer stream.Close()
 
