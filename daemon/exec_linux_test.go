0a30edf4081e62029f1a9cccc0f8d7e8d6db3f51
Leandro Motta Barros <lmb@stackedboxes.org>
2022-11-01 20:23:49 -0300

    Fix compilation errors after dependencies update
    Some of the updated dependencies introduced breaking changes. This
    commit deals with the compilation errors that thus arose in our code.
    
    Signed-off-by: Leandro Motta Barros <leandro@balena.io>
    Change-type: patch

--- daemon/exec_linux_test.go
+++ daemon/exec_linux_test.go
@@ -8,6 +8,7 @@ import (
 
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/container"
+	"github.com/docker/docker/daemon/config"
 	"github.com/docker/docker/daemon/exec"
 	"github.com/opencontainers/runc/libcontainer/apparmor"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
@@ -18,8 +19,11 @@ func TestExecSetPlatformOpt(t *testing.T) {
 	if !apparmor.IsEnabled() {
 		t.Skip("requires AppArmor to be enabled")
 	}
-	d := &Daemon{}
-	c := &container.Container{AppArmorProfile: "my-custom-profile"}
+	d := &Daemon{configStore: &config.Config{}}
+	c := &container.Container{
+		AppArmorProfile: "my-custom-profile",
+		HostConfig:      &containertypes.HostConfig{Privileged: false},
+	}
 	ec := &exec.Config{}
 	p := &specs.Process{}
 
@@ -34,21 +38,24 @@ func TestExecSetPlatformOpt(t *testing.T) {
 //
 // This behavior may change in future, but test for the behavior to prevent it
 // from being changed accidentally.
+//
+// balenaEngine: this test was failing after we upgraded several components
+// while updating containerd to 1.6.6. We changed it so it resembles the
+// following test case in the more recent Moby codebase:
+// https://github.com/moby/moby/blob/572ca799db4b67b7be35904e487f0cc51c3f9f06/daemon/exec_linux_test.go#L37-L39
 func TestExecSetPlatformOptPrivileged(t *testing.T) {
 	if !apparmor.IsEnabled() {
 		t.Skip("requires AppArmor to be enabled")
 	}
-	d := &Daemon{}
-	c := &container.Container{AppArmorProfile: "my-custom-profile"}
-	ec := &exec.Config{Privileged: true}
+	d := &Daemon{configStore: &config.Config{}}
+	c := &container.Container{
+		AppArmorProfile: "",
+		HostConfig:      &containertypes.HostConfig{Privileged: true},
+	}
+	ec := &exec.Config{Privileged: false}
 	p := &specs.Process{}
 
 	err := d.execSetPlatformOpt(c, ec, p)
-	assert.NilError(t, err)
-	assert.Equal(t, "my-custom-profile", p.ApparmorProfile)
-
-	c.HostConfig = &containertypes.HostConfig{Privileged: true}
-	err = d.execSetPlatformOpt(c, ec, p)
 	assert.NilError(t, err)
 	assert.Equal(t, unconfinedAppArmorProfile, p.ApparmorProfile)
 }
