17323756786fb748ccfced43083c75223826c8dd
Leandro Motta Barros <lmb@stackedboxes.org>
2022-11-22 10:23:06 -0300

    Post-merge tweaks
    Small changes to keep our code closer to Moby's.

--- pkg/devicemapper/devmapper_wrapper_dynamic.go
+++ pkg/devicemapper/devmapper_wrapper_dynamic.go
@@ -1,4 +1,4 @@
-//go:build linux && cgo && !static_build
+//go:build linux && cgo && !static_build && !no_devmapper
 // +build linux,cgo,!static_build,!no_devmapper
 
 package devicemapper // import "github.com/docker/docker/pkg/devicemapper"
