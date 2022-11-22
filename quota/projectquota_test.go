7284a685b02f0308e888279d6a79571556e4ff0e
Leandro Motta Barros <lmb@stackedboxes.org>
2022-11-22 10:23:06 -0300

    Post-merge tweaks
    Small changes to keep our code closer to Moby's.
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- quota/projectquota_test.go
+++ quota/projectquota_test.go
@@ -1,5 +1,5 @@
-//go:build linux
-// +build linux
+//go:build linux && !exclude_disk_quota && cgo
+// +build linux,!exclude_disk_quota,cgo
 
 package quota // import "github.com/docker/docker/quota"
 
