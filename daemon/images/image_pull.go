1b2f863879c7b58246086b0234db3f0d486055a1
Leandro Motta Barros <lmb@stackedboxes.org>
2023-01-18 11:15:22 -0300

    Set the delta image store, fix delta-based HUPs
    We apparently have broken this during the 20.10 merge. Not setting the
    delta image store breaks delta-based balenaOS updates (HUPs).
    
    Signed-off-by: Leandro Motta Barros <leandro@balena.io>
    Change-type: patch

--- daemon/images/image_pull.go
+++ daemon/images/image_pull.go
@@ -106,7 +106,7 @@ func (i *ImageService) pullImageWithReference(ctx context.Context, ref reference
 		leases:       i.leases,
 	}
 	imageStore := &imageStoreForPull{
-		ImageConfigStore: distribution.NewImageConfigStoreFromStore(i.imageStore),
+		ImageConfigStore: distribution.NewImageConfigStoreFromStore(i.imageStore, i.deltaStore),
 		ingested:         cs,
 		leases:           i.leases,
 	}
@@ -120,7 +120,6 @@ func (i *ImageService) pullImageWithReference(ctx context.Context, ref reference
 			ImageEventLogger: i.LogImageEvent,
 			MetadataStore:    i.distributionMetadataStore,
 			ImageStore:       imageStore,
-			ImageStore:       distribution.NewImageConfigStoreFromStore(i.imageStore, i.deltaStore),
 			ReferenceStore:   i.referenceStore,
 		},
 		DownloadManager: i.downloadManager,
