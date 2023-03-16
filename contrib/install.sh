f1ae01f3a08a1e1cc7f14be90bb5869255354c79
Leandro Motta Barros <lmb@stackedboxes.org>
2023-03-16 17:25:11 -0300

    Installer: Make the script POSIX-compliant
    There were various usages of Bash-specific features. As a result, the
    script would work correctly only on OSes that have `sh` as an alias to
    `bash`. It would fail on Ubuntu, for example.
    
    Signed-off-by: Leandro Motta Barros <leandro@balena.io>
    Change-type: patch

--- contrib/install.sh
+++ contrib/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eo pipefail
+set -e
 
 tag="v20.10.19"
 tag=$(echo "$tag" | sed 's|+|.|g')
@@ -8,14 +8,14 @@ tag=$(echo "$tag" | sed 's|+|.|g')
 # Check and warn about missing required commands before doing any actual work.
 abort=0
 for cmd in curl tar; do
-	if [[ -z $(command -v $cmd) ]]; then
+	if [ -z "$(command -v $cmd)" ]; then
 		cat >&2 <<-EOF
 		Error: unable to find required command: $cmd
 		EOF
 		abort=1
 	fi
 done
-[ $abort == 1 ] && exit 1
+[ $abort = 1 ] && exit 1
 
 sudo=
 if [ "$(id -u)" -ne 0 ]; then
