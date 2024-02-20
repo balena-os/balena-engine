ee925a558eb8af009b7cf602b87d4817d85a8237
Ken Bannister <kb2ma@runbox.com>
2024-02-19 19:39:36 -0500

    Re-vendor for addition of libnetwork Macvlan driver
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- vendor/github.com/docker/libnetwork/drivers_linux.go
+++ vendor/github.com/docker/libnetwork/drivers_linux.go
@@ -3,6 +3,7 @@ package libnetwork
 import (
 	"github.com/docker/libnetwork/drivers/bridge"
 	"github.com/docker/libnetwork/drivers/host"
+	"github.com/docker/libnetwork/drivers/macvlan"
 	"github.com/docker/libnetwork/drivers/null"
 	"github.com/docker/libnetwork/drivers/remote"
 )
@@ -11,6 +12,7 @@ func getInitializers(experimental bool) []initializer {
 	in := []initializer{
 		{bridge.Init, "bridge"},
 		{host.Init, "host"},
+		{macvlan.Init, "macvlan"},
 		{null.Init, "null"},
 		{remote.Init, "remote"},
 	}
