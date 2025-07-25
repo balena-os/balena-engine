60c7fd3e5428a8fe0c6b918e41ccc065515805bf
Alex Gonzalez <alexg@balena.io>
2025-07-25 16:11:33 +0200

    testutil: daemon: default to not specifying containerd socket
    In commit b96a0c775400821d80972619fbfe6a2070f3e9ba, a default containerd
    socket is passed to the daemon by default. This was introduced for
    build kit support which balena engine does not yet support.
    
    Passing the containerd socker makes balena engine not start and the
    integration tests to fail.
    
    Signed-off-by: Alex Gonzalez <alexg@balena.io>
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- testutil/daemon/daemon.go
+++ testutil/daemon/daemon.go
@@ -135,7 +135,7 @@ func NewDaemon(workingDir string, ops ...Option) (*Daemon, error) {
 		swarmListenAddr:  defaultSwarmListenAddr,
 		SwarmPort:        DefaultSwarmPort,
 		log:              nopLog{},
-		containerdSocket: defaultContainerdSocket,
+		containerdSocket: "",
 	}
 
 	for _, op := range ops {
