c87255ba24b9fcd4939495c14694e68befab3852
Kyle Harding <kyle@balena.io>
2025-12-04 17:48:21 -0500

    cmd/mobynit: update mount import to moby/sys/mountinfo
    The pkg/mount package was removed from moby/moby and moved to
    github.com/moby/sys/mountinfo. Update the import accordingly.
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- cmd/mobynit/main.go
+++ cmd/mobynit/main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/docker/docker/pkg/mount"
+	"github.com/moby/sys/mountinfo"
 	"golang.org/x/sys/unix"
 	"github.com/docker/docker/cmd/mobynit/hostapp"
 )
@@ -62,7 +62,7 @@ func main() {
 	flag.Parse()
 
 	// Any mounts done by initrd will be transfered in the new root
-	mounts, err := mount.GetMounts(nil)
+	mounts, err := mountinfo.GetMounts(nil)
 	if err != nil {
 		log.Fatal("could not get mounts:", err)
 	}
