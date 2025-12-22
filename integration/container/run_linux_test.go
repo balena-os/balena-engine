e189e7bd25e300d810d65d5dec4f33fa9c9d2efe
Kyle Harding <kyle@balena.io>
2025-12-22 13:41:06 -0500

    integration: skip TestRunWithAlternativeContainerdShim
    Skip test that requires arbitrary shim name support. The balena-engine
    busybox binary has hardcoded command names in main.go and cannot
    dynamically dispatch to shims with custom names like "realfake-v42".
    
    Upstream docker discovers shims via PATH lookup, but balena's multicall
    binary architecture only supports explicitly defined command aliases.
    
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- integration/container/run_linux_test.go
+++ integration/container/run_linux_test.go
@@ -219,6 +219,7 @@ func TestRunConsoleSize(t *testing.T) {
 }
 
 func TestRunWithAlternativeContainerdShim(t *testing.T) {
+	t.Skip("Skipping test that requires custom shim name support")
 	skip.If(t, testEnv.IsRemoteDaemon)
 	skip.If(t, testEnv.DaemonInfo.OSType != "linux")
 
@@ -241,7 +242,7 @@ func TestRunWithAlternativeContainerdShim(t *testing.T) {
 	assert.Assert(t, os.Chmod(shimDir, 0777))
 	shimDir, err = filepath.Abs(shimDir)
 	assert.Assert(t, err)
-	assert.Assert(t, os.Symlink(realShimPath, filepath.Join(shimDir, "containerd-shim-realfake-v42")))
+	assert.Assert(t, os.Symlink(realShimPath, filepath.Join(shimDir, "balena-containerd-shim-realfake-v42")))
 
 	d := daemon.New(t,
 		daemon.WithEnvVars("PATH="+shimDir+":"+os.Getenv("PATH")),
