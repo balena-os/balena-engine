9c406e24184b18738fc3581d31284320742f92f7
Shaun Mulligan <switchflip.shaun@gmail.com>
2026-01-14 15:21:53 +0000

    build: support bind mounts during docker build
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- builder/dockerfile/internals.go
+++ builder/dockerfile/internals.go
@@ -10,8 +10,9 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/containerd/log"
+"github.com/containerd/log"
 	"github.com/containerd/platforms"
+	"github.com/docker/cli/cli/compose/loader"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/backend"
 	"github.com/docker/docker/api/types/container"
@@ -349,6 +350,14 @@ func (b *Builder) probeAndCreate(ctx context.Context, dispatchState *dispatchSta
 }
 
 func (b *Builder) create(ctx context.Context, runConfig *container.Config) (string, error) {
+	// only allow bind-mounting during build
+	for _, bind := range b.options.Volumes {
+		parsed, _ := loader.ParseVolume(bind)
+		if parsed.Source == "" {
+			return "", fmt.Errorf("Cannot use non-bind mount during build: %s", bind)
+		}
+	}
+
 	log.G(ctx).Debugf("[BUILDER] Command to be executed: %v", runConfig.Cmd)
 
 	hostConfig := hostConfigFromOptions(b.options)
@@ -394,6 +403,7 @@ func hostConfigFromOptions(options *types.ImageBuildOptions) *container.HostConf
 		// Set a log config to override any default value set on the daemon
 		LogConfig:  defaultLogConfig,
 		ExtraHosts: options.ExtraHosts,
+		Binds:      options.Volumes,
 	}
 	return hc
 }
