b420d8fd3fe8866fce6219b8cab11eb1c77a965f
Petros Angelatos <petrosagg@gmail.com>
2026-01-17 08:49:57 +0000

    libnetwork: disable macvlan,overlay network drivers
    These network drivers are not used in balenaOS and can be disabled
    to reduce binary size and attack surface.
    
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- libnetwork/drivers_linux.go
+++ libnetwork/drivers_linux.go
@@ -7,9 +7,7 @@ import (
 	"github.com/docker/docker/libnetwork/drivers/bridge"
 	"github.com/docker/docker/libnetwork/drivers/host"
 	"github.com/docker/docker/libnetwork/drivers/ipvlan"
-	"github.com/docker/docker/libnetwork/drivers/macvlan"
 	"github.com/docker/docker/libnetwork/drivers/null"
-	"github.com/docker/docker/libnetwork/drivers/overlay"
 )
 
 func registerNetworkDrivers(r driverapi.Registerer, driverConfig func(string) map[string]interface{}) error {
@@ -24,9 +22,7 @@ func registerNetworkDrivers(r driverapi.Registerer, driverConfig func(string) ma
 		{ntype: bridge.NetworkType, register: bridge.Register},
 		{ntype: host.NetworkType, register: noConfig(host.Register)},
 		{ntype: ipvlan.NetworkType, register: ipvlan.Register},
-		{ntype: macvlan.NetworkType, register: macvlan.Register},
 		{ntype: null.NetworkType, register: noConfig(null.Register)},
-		{ntype: overlay.NetworkType, register: overlay.Register},
 	} {
 		if err := nr.register(r, driverConfig(nr.ntype)); err != nil {
 			return fmt.Errorf("failed to register %q driver: %w", nr.ntype, err)
